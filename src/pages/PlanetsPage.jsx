67145b6458233feb958a2ae0359e6e1f3d3c81e1
Ilya Kasperovich <kasper.ii2108@gmail.com>
2020-06-14 17:26:35 +0300

    added planets & ships pages

--- src/pages/PlanetsPage.jsx
+++ src/pages/PlanetsPage.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Table from "../components/common/Table"
+import Form from '../components/common/Form'
 
-export default function PlanetsPage () {
+const data = [
+  {name: 'Earth', galaxy: 'Milky Way', id: '1'},
+  {name: 'Utapau', galaxy: 'Kessil', id: '2'},
+  {name: 'Golm', galaxy: 'Bot', id: '3'}
+]
+
+const columns = Object.keys(data[0]);
+
+export default function PlanetsPage() {
+  const [planets, setPlanets] = useState(data);
+
+  const handleAppPlanet = (planetData) => {
+      const data = [...planets, planetData];
+      setPlanets(data)
+  }
+
+  const getInitialData = () => {
+      return columns.reduce((cols, columnName) => {
+          cols[columnName] = "";
+          return cols;
+      }, {})
+  }
+
+  const handleDeletePlanet = (event) => {
+    const id = event.currentTarget.id;
+    const data = planets.filter(item => item.id !== id)
+    setPlanets(data)
+  }
+
   return (
-    <div>PlanetsPage</div>
-  )
+      <div className="container">
+        <div className="h1">Planets</div>
+          <Table
+              data={planets}
+              columns={columns}
+              tableDescriptor="Planets"
+              onDelete={handleDeletePlanet}
+          />
+          <Form
+              initialData={getInitialData()}
+              columns={columns}
+              onAddData={handleAppPlanet}
+          />
+      </div>
+  );
 }
--- src/pages/StarshipsPage.jsx
+++ src/pages/StarshipsPage.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Table from "../components/common/Table"
+import Form from '../components/common/Form'
 
-export default function StarshipsPage () {
+const data = [
+  {owner: 'Mark', model: 'SID', id: '1'},
+  {owner: 'Darth Vader', model: 'Super', id: '2'},
+  {owner: 'Steve', model: 'Snowspeeder', id: '3'}
+]
+
+const columns = Object.keys(data[0]);
+
+export default function StarshipsPage() {
+  const [starships, setStarships] = useState(data);
+
+  const handleAppStarship = (starshipData) => {
+      const data = [...starships, starshipData];
+      setStarships(data)
+  }
+
+  const getInitialData = () => {
+      return columns.reduce((cols, columnName) => {
+          cols[columnName] = "";
+          return cols;
+      }, {})
+  }
+
+  const handleDeleteStarship = (event) => {
+    const id = event.currentTarget.id;
+    const data = starships.filter(item => item.id !== id)
+    setStarships(data)
+  }
+
   return (
-    <div>StarshipsPage</div>
-  )
+      <div className="container">
+        <div className="h1">Starships</div>
+          <Table
+              data={starships}
+              columns={columns}
+              tableDescriptor="Starships"
+              onDelete={handleDeleteStarship}
+          />
+          <Form
+              initialData={getInitialData()}
+              columns={columns}
+              onAddData={handleAppStarship}
+          />
+      </div>
+  );
 }
